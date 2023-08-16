3059f384b6d27d622d5bb77c6bc0e3fd7d7cdd5e
Renan Ventura <138172820+RenanVentura@users.noreply.github.com>
2023-08-16 17:17:13 +0000

    adiciona scripst `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function home (){
-    return <h1>Ana Laura, eu te amo com todo meu coração ❤ </h1>
+function home() {
+  return <h1>Ana Laura, eu te amo com todo meu coração ❤ </h1>;
 }
 
 export default home;
